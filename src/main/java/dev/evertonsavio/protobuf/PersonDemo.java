0cbdf7baebe97cc7cbea98e8d73557d9b29f5659
havyx <everluca@outlook.com>
2021-03-21 11:02:49 -0300

    :sparkles: person proto object working

--- src/main/java/dev/evertonsavio/protobuf/PersonDemo.java
+++ src/main/java/dev/evertonsavio/protobuf/PersonDemo.java
@@ -1,10 +1,18 @@
 package dev.evertonsavio.protobuf;
 
+import dev.evertonsavio.protobuf.models.Person;
+
 public class PersonDemo {
 
     public static void main(String[] args) {
 
     //
+        Person savio = Person.newBuilder()
+                .setName("Savio")
+                .setAge(34)
+                .build();
+
+        System.out.println(savio.toString());
 
     }
 
