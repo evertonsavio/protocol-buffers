9866f4b545a67a59713d4fbacda423f57d92f878
havyx <everluca@outlook.com>
2021-03-21 14:44:32 -0300

    :sparkles: serialization & deserialization

--- src/main/java/dev/evertonsavio/protobuf/PersonDemo.java
+++ src/main/java/dev/evertonsavio/protobuf/PersonDemo.java
@@ -2,23 +2,27 @@ package dev.evertonsavio.protobuf;
 
 import dev.evertonsavio.protobuf.models.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-    //
         Person savio = Person.newBuilder()
                 .setName("Savio")
                 .setAge(34)
                 .build();
 
-        Person savio2 = Person.newBuilder()
-                .setName("Savio2")
-                .setAge(34)
-                .build();
+        Path path = Paths.get("Savio.ser");
+        Files.write(path, savio.toByteArray());
 
-        System.out.println(savio.equals(savio2));
+        byte[] bytes = Files.readAllBytes(path);
+        Person newSavio = Person.parseFrom(bytes);
 
+        System.out.println(newSavio.toString());
     }
 
 }
