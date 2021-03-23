b9cd298b6ce988f13ffe1623b4c71a915fd6cd44
havyx <everluca@outlook.com>
2021-03-23 19:35:07 -0300

    :sparkles: proto default values

---
+++ src/main/proto/DefaultValueDemo.java
@@ -0,0 +1,11 @@
+import dev.evertonsavio.protobuf.models.Person;
+
+public class DefaultValueDemo {
+
+    public static void main(String[] args) {
+
+        Person person = Person.newBuilder().build();
+        System.out.println("CITY:" + person.getAddress().getCity());
+        System.out.println(person.hasAddress());
+    }
+}
